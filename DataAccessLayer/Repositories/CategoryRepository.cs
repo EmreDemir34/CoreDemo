18bc3687a27fb141af19028603ab81ed4451d8af
emredemir3498 <emredemir3498@icloud.com>
2022-04-09 23:38:08 +0300

    Add project files.

---
+++ DataAccessLayer/Repositories/CategoryRepository.cs
@@ -0,0 +1,43 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class CategoryRepository : ICategoryDal
+    {
+        Context c = new Context();
+        public void AddCategory(Category addcategoryparameter)
+        {
+            c.Add(addcategoryparameter);
+            c.SaveChanges();
+        }
+
+        public void DeleteCategory(Category deletecategoryparameter)
+        {
+            c.Remove(deletecategoryparameter);
+            c.SaveChanges();
+        }
+
+        public Category GetById(int id)
+        {
+            return c.Categories.Find(id);
+        }
+
+        public List<Category> ListAllCategory()
+        {
+            return c.Categories.ToList();
+        }
+
+        public void UpdateCategory(Category updatecategoryparameter)
+        {
+            c.Update(updatecategoryparameter);
+            c.SaveChanges();
+        }
+    }
+}
